f564688837e476e4672bfdc8396b5e04171c29c9
marayl <marayl@a3a97f7c-8e16-0410-9893-1d5f7d0f8f6e>
2006-11-18 19:51:41 +0000

    Added exceptions to ansi strictness.
    git-svn-id: https://aug.svn.sourceforge.net/svnroot/aug/trunk@145 a3a97f7c-8e16-0410-9893-1d5f7d0f8f6e

--- src/c/augutil/path.c
+++ src/c/augutil/path.c
@@ -1,6 +1,7 @@
 /* Copyright (c) 2004-2006, Mark Aylett <mark@emantic.co.uk>
    See the file COPYING for copying permission.
 */
+#undef __STRICT_ANSI__     /* _fullpath() */
 #define AUGUTIL_BUILD
 #include "augutil/path.h"
 
