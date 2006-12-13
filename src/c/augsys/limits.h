f066820f6ba5862e87496a718c530f0ad62b8f8a
marayl <marayl@a3a97f7c-8e16-0410-9893-1d5f7d0f8f6e>
2006-12-13 16:26:35 +0000

    Removed unwanted tests from configure.in.
    git-svn-id: https://aug.svn.sourceforge.net/svnroot/aug/trunk@165 a3a97f7c-8e16-0410-9893-1d5f7d0f8f6e

--- src/c/augsys/limits.h
+++ src/c/augsys/limits.h
@@ -4,20 +4,20 @@
 #ifndef AUGSYS_LIMITS_H
 #define AUGSYS_LIMITS_H
 
-#if !defined(WIN32)
+#if !defined(_WIN32)
 # include <limits.h>
 # if !defined(PATH_MAX)
 #  define AUG_PATH_MAX 255
 # else /* PATH_MAX */
 #  define AUG_PATH_MAX PATH_MAX
 # endif /* PATH_MAX */
-#else /* WIN32 */
+#else /* _WIN32 */
 # include <stdlib.h>
 # if !defined(_MAX_PATH)
 #  define AUG_PATH_MAX 260
 # else /* _MAX_PATH */
 #  define AUG_PATH_MAX _MAX_PATH
 # endif /* _MAX_PATH */
-#endif /* WIN32 */
+#endif /* _WIN32 */
 
 #endif /* AUGSYS_LIMITS_H */
