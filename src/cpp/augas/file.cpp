4e754237e4d0504c551ecb2ef771bbb50dee242e
marayl <marayl@a3a97f7c-8e16-0410-9893-1d5f7d0f8f6e>
2007-01-10 20:32:29 +0000

    Added missing files.
    git-svn-id: https://aug.svn.sourceforge.net/svnroot/aug/trunk@188 a3a97f7c-8e16-0410-9893-1d5f7d0f8f6e

---
+++ src/cpp/augas/file.cpp
@@ -0,0 +1,11 @@
+/* Copyright (c) 2004-2007, Mark Aylett <mark@emantic.co.uk>
+   See the file COPYING for copying permission.
+*/
+#define AUGAS_BUILD
+#include "augas/file.hpp"
+
+using namespace augas;
+
+file_base::~file_base() AUG_NOTHROW
+{
+}
---
+++ src/cpp/augas/file.hpp
@@ -0,0 +1,71 @@
+/* Copyright (c) 2004-2007, Mark Aylett <mark@emantic.co.uk>
+   See the file COPYING for copying permission.
+*/
+#ifndef AUGAS_FILE_HPP
+#define AUGAS_FILE_HPP
+
+#include "augas.h"
+#include "augas/sess.hpp"
+
+#include "augsyspp/smartptr.hpp"
+
+namespace augas {
+
+    class file_base {
+    public:
+        typedef augas_file ctype;
+    private:
+
+        virtual augas_file&
+        do_file() = 0;
+
+        virtual int
+        do_fd() const = 0;
+
+        virtual const augas_file&
+        do_file() const = 0;
+
+        virtual const sessptr&
+        do_sess() const = 0;
+
+    public:
+        virtual
+        ~file_base() AUG_NOTHROW;
+
+        int
+        fd() const
+        {
+            return do_fd();
+        }
+
+        augas_id
+        id() const
+        {
+            return do_file().id_;
+        }
+
+        void*
+        user() const
+        {
+            return do_file().user_;
+        }
+
+        const sessptr&
+        sess() const
+        {
+            return do_sess();
+        }
+        operator augas_file&()
+        {
+            return do_file();
+        }
+        operator const augas_file&() const
+        {
+            return do_file();
+        }
+    };
+
+    typedef aug::smartptr<file_base> fileptr;
+}
+
+#endif // AUGAS_FILE_HPP
