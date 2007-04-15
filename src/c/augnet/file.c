808e913e8ee81f2c395d525b14bcfdc26faab8d1
marayl <marayl@a3a97f7c-8e16-0410-9893-1d5f7d0f8f6e>
2007-04-15 08:51:52 +0000

    Improved file implementation.
    git-svn-id: https://aug.svn.sourceforge.net/svnroot/aug/trunk@321 a3a97f7c-8e16-0410-9893-1d5f7d0f8f6e

--- src/c/augnet/file.c
+++ src/c/augnet/file.c
@@ -17,7 +17,7 @@ AUG_RCSID("$Id$");
 
 struct aug_file_ {
     AUG_ENTRY(aug_file_);
-    int fd_;
+    int trash_, fd_;
     aug_filecb_t cb_;
     struct aug_var var_;
 };
@@ -25,12 +25,68 @@ struct aug_file_ {
 static struct aug_files free_ = AUG_HEAD_INITIALIZER(free_);
 AUG_ALLOCATOR(allocate_, &free_, aug_file_, 64)
 
+/* When added to the front of a list, this marker indicates that the list is
+   locked. */
+
+static struct aug_file_ lockfile_ = { { 0 }, 0, -1, NULL, AUG_VARNULL };
+#define LOCKFILE_ (&lockfile_)
+
+static void
+lock_(struct aug_files* files)
+{
+    /* Added lock marker to front. */
+
+    AUG_INSERT_HEAD(files, LOCKFILE_);
+}
+
+static void
+unlock_(struct aug_files* files)
+{
+    struct aug_file_* it, ** prev;
+    struct aug_files trash;
+    AUG_INIT(&trash);
+
+    /* Remove lock marker from front. */
+
+    AUG_REMOVE_HEAD(files);
+
+    /* Moved all trashed items to trash list. */
+
+    prev = &AUG_FIRST(files);
+    while ((it = *prev)) {
+
+        if (it->trash_) {
+
+            AUG_REMOVE_PREVPTR(it, prev, files);
+            AUG_INSERT_TAIL(&trash, it);
+
+        } else
+            prev = &AUG_NEXT(it);
+    }
+
+    /* Free trashed items. */
+
+    if (!AUG_EMPTY(&trash)) {
+
+        aug_lock();
+        AUG_CONCAT(&free_, &trash);
+        aug_unlock();
+    }
+}
+
+static int
+locked_(struct aug_files* files)
+{
+    return AUG_FIRST(files) == LOCKFILE_;
+}
+
 AUGNET_API int
 aug_destroyfiles(struct aug_files* files)
 {
     struct aug_file_* it;
     AUG_FOREACH(it, files)
-        aug_destroyvar(&it->var_);
+        if (!it->trash_)
+            aug_destroyvar(&it->var_);
 
     if (!AUG_EMPTY(files)) {
 
@@ -54,6 +110,7 @@ aug_insertfile(struct aug_files* files, int fd, aug_filecb_t cb,
     }
     aug_unlock();
 
+    file->trash_ = 0;
     file->fd_ = fd;
     file->cb_ = cb;
     aug_setvar(&file->var_, var);
@@ -65,10 +122,11 @@ aug_insertfile(struct aug_files* files, int fd, aug_filecb_t cb,
 AUGNET_API int
 aug_removefile(struct aug_files* files, int fd)
 {
-    struct aug_file_* it;
+    /* Locate the matching entry. */
 
+    struct aug_file_* it;
     AUG_FOREACH(it, files)
-        if (it->fd_ == fd)
+        if (it->fd_ == fd && !it->trash_)
             break;
 
     if (!it) {
@@ -77,12 +135,24 @@ aug_removefile(struct aug_files* files, int fd)
         return -1;
     }
 
-    AUG_REMOVE(files, it, aug_file_);
-
     aug_destroyvar(&it->var_);
-    aug_lock();
-    AUG_INSERT_TAIL(&free_, it);
-    aug_unlock();
+
+    if (locked_(files)) {
+
+        /* Items are merely marked for removal while a aug_foreachfile()
+           operation is in progress. */
+
+        it->trash_ = 1;
+
+    } else {
+
+        /* Otherwise, move to free list immediately. */
+
+        AUG_REMOVE(files, it, aug_file_);
+        aug_lock();
+        AUG_INSERT_TAIL(&free_, it);
+        aug_unlock();
+    }
 
     return 0;
 }
@@ -90,33 +160,47 @@ aug_removefile(struct aug_files* files, int fd)
 AUGNET_API int
 aug_foreachfile(struct aug_files* files)
 {
-    struct aug_file_* it, ** prev;
-    struct aug_files tail;
-    AUG_INIT(&tail);
+    struct aug_file_* it, * end;
+    int locked = locked_(files);
 
-    prev = &AUG_FIRST(files);
-    while ((it = *prev)) {
+    /* Ensure list is locked so that no entries are actually removed during
+       iteration. */
 
-        if (!(it->cb_(&it->var_, it->fd_, &tail))) {
+    if (!locked)
+        lock_(files);
 
-            AUG_REMOVE_PREVPTR(it, prev, files);
+    end = AUG_LAST(files, aug_file_);
+    AUG_FOREACH(it, files) {
+
+        /* Ignore lock marker and trashed entries. */
+
+        if (it == LOCKFILE_ || it->trash_)
+            continue;
+
+        if (!(it->cb_(&it->var_, it->fd_, files))) {
 
             aug_destroyvar(&it->var_);
-            aug_lock();
-            AUG_INSERT_TAIL(&free_, it);
-            aug_unlock();
+            it->trash_ = 1;
+        }
 
-        } else
-            prev = &AUG_NEXT(it);
+        /* Avoid iterating over items beyond those that were there at the
+           start. */
+
+        if (it == end)
+            break;
     }
 
-    AUG_CONCAT(files, &tail);
+    if (!locked /* On entry. */) {
 
-    /* Rotate files to promote fairness. */
+        unlock_(files);
 
-    if (!(it = AUG_FIRST(files))) {
-        AUG_REMOVE_HEAD(files);
-        AUG_INSERT_TAIL(files, it);
+        /* Rotate files to promote fairness - this must only be done once the
+           lock marker has been removed from the front of the list. */
+
+        if (!(it = AUG_FIRST(files))) {
+            AUG_REMOVE_HEAD(files);
+            AUG_INSERT_TAIL(files, it);
+        }
     }
     return 0;
 }
@@ -127,7 +211,7 @@ aug_filevar(struct aug_files* files, int fd)
     struct aug_file_* it;
 
     AUG_FOREACH(it, files)
-        if (it->fd_ == fd)
+        if (it->fd_ == fd && !it->trash_)
             return &it->var_;
 
     return NULL;
