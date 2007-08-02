0f0db273843c0c8a02fb1064cb2b5fe1b187eab4
marayl <marayl@a3a97f7c-8e16-0410-9893-1d5f7d0f8f6e>
2007-08-02 20:45:40 +0000

    Added bench shell script.
    git-svn-id: https://aug.svn.sourceforge.net/svnroot/aug/trunk@450 a3a97f7c-8e16-0410-9893-1d5f7d0f8f6e

---
+++ example/augrt/bench.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+HOST=${1:-'localhost'}
+
+function bench() {
+    cat <<EOF >$1.conf
+rundir = .
+logdir = .
+loglevel = 4
+
+sessions = bench
+
+session.bench.module = client
+session.bench.sendv = 0
+session.bench.host = $HOST
+session.bench.serv = 7000
+session.bench.conns = $1
+session.bench.echos = 5000
+
+module.client.path = ./modclient
+EOF
+
+    $AUG_HOME/bin/daug -f $1.conf test
+    mv bench.dat $1.dat
+    rm -f $1.conf
+}
+
+for n in 4 8 16 32 64 128; do
+    bench $n
+done
