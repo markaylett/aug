586fbad641f73ea4f5a65a2227812891d90d1478
Mark Aylett <mark@emantic.co.uk>
2007-10-18 19:08:13 +0100

    Renamed MSI from daug to aug.

---
+++ vc7/aug.bat
@@ -0,0 +1,2 @@
+devenv.com aug.sln /rebuild Debug /project setup
+devenv.com aug.sln /rebuild Release /project setup
--- vc7/daug.bat
+++
@@ -1,2 +0,0 @@
-devenv.com daug.sln /rebuild Debug /project setup
-devenv.com daug.sln /rebuild Release /project setup
