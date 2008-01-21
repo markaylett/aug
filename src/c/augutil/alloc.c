1da48e1add703c0a350e9c492eb6b12ba08fc8ba
Mark Aylett <mark@emantic.co.uk>
2008-01-21 16:01:35 +0000

    Added aug allocator functions.

--- src/c/augutil/alloc.c
+++ src/c/augutil/alloc.c
@@ -10,37 +10,131 @@ AUG_RCSID("$Id$");
 #include "augsys/errinfo.h"
 #include "augsys/errno.h"
 #include "augsys/lock.h"
-#include <stdlib.h>
 
-static struct aug_allocs free_ = AUG_HEAD_INITIALIZER(free_);
-AUG_ALLOCATOR(allocate_, &free_, aug_alloc, 64)
+#include <stdlib.h>
+#include <string.h>
+
+enum {
+    ALLOC32_,
+    ALLOC64_,
+    MALLOC_
+};
+
+static struct aug_allocs32 free32_ = AUG_HEAD_INITIALIZER(free32_);
+AUG_ALLOCATOR(allocate32_, &free32_, aug_alloc32, 64)
+
+static struct aug_allocs64 free64_ = AUG_HEAD_INITIALIZER(free64_);
+AUG_ALLOCATOR(allocate64_, &free64_, aug_alloc64, 64)
 
 static void
-destroyallocs_(struct aug_allocs* allocs)
+freemem32_(void* ptr)
 {
-    if (!AUG_EMPTY(allocs)) {
+    char* mem = ptr;
+    struct aug_alloc32* alloc = (struct aug_alloc32*)
+        (mem - offsetof(struct aug_alloc32, mem_));
+    aug_destroyalloc32(alloc);
+}
+
+static void
+freemem64_(void* ptr)
+{
+    char* mem = ptr;
+    struct aug_alloc64* alloc = (struct aug_alloc64*)
+        (mem - offsetof(struct aug_alloc64, mem_));
+    aug_destroyalloc64(alloc);
+}
+
+AUGUTIL_API void
+aug_destroyallocs32(struct aug_allocs32* allocs32)
+{
+    if (!AUG_EMPTY(allocs32)) {
         aug_lock();
-        AUG_CONCAT(&free_, allocs);
+        AUG_CONCAT(&free32_, allocs32);
         aug_unlock();
     }
 }
 
-static void
-destroyalloc_(struct aug_alloc* alloc)
+AUGUTIL_API void
+aug_destroyalloc32(struct aug_alloc32* alloc32)
 {
     aug_lock();
-    AUG_INSERT_TAIL(&free_, alloc);
+    AUG_INSERT_TAIL(&free32_, alloc32);
     aug_unlock();
 }
 
-static struct aug_alloc*
-createalloc_(void)
+AUGUTIL_API struct aug_alloc32*
+aug_createalloc32(void)
 {
-    struct aug_alloc* alloc;
+    struct aug_alloc32* alloc32;
 
     aug_lock();
-    alloc = allocate_();
+    alloc32 = allocate32_();
     aug_unlock();
 
-    return alloc->mem_;
+    return alloc32;
+}
+
+AUGUTIL_API void
+aug_destroyallocs64(struct aug_allocs64* allocs64)
+{
+    if (!AUG_EMPTY(allocs64)) {
+        aug_lock();
+        AUG_CONCAT(&free64_, allocs64);
+        aug_unlock();
+    }
+}
+
+AUGUTIL_API void
+aug_destroyalloc64(struct aug_alloc64* alloc64)
+{
+    aug_lock();
+    AUG_INSERT_TAIL(&free64_, alloc64);
+    aug_unlock();
+}
+
+AUGUTIL_API struct aug_alloc64*
+aug_createalloc64(void)
+{
+    struct aug_alloc64* alloc64;
+
+    aug_lock();
+    alloc64 = allocate64_();
+    aug_unlock();
+
+    return alloc64;
+}
+
+AUGUTIL_API void
+aug_free(void* ptr)
+{
+    static void (*free_[3])(void*) = {
+        freemem32_,
+        freemem64_,
+        free
+    };
+    char* mem = (char*)ptr - 1;
+    free_[(size_t)mem[0]](mem);
+}
+
+AUGUTIL_API void*
+aug_alloc(size_t size)
+{
+    char* mem = NULL;
+    if (size <= 32 - 1) {
+        struct aug_alloc32* alloc32 = aug_createalloc32();
+        if (alloc32) {
+            mem = alloc32->mem_;
+            *mem++ = ALLOC32_;
+        }
+    } else if (size <= 64 - 1) {
+        struct aug_alloc64* alloc64 = aug_createalloc64();
+        if (alloc64) {
+            mem = alloc64->mem_;
+            *mem++ = ALLOC64_;
+        }
+    } else {
+        if ((mem = malloc(size + sizeof(void (*)(void*)))))
+            *mem++ = MALLOC_;
+    }
+    return mem;
 }
--- src/c/augutil/alloc.h
+++ src/c/augutil/alloc.h
@@ -7,17 +7,48 @@
 /**
  * @file augutil/alloc.h
  *
- * Small object allocator.
+ * Slab allocators for small objects.
  */
 
 #include "augutil/config.h"
 #include "augutil/list.h"
 
-struct aug_alloc {
-    AUG_ENTRY(aug_alloc);
+struct aug_alloc32 {
+    AUG_ENTRY(aug_alloc32);
+    char mem_[32];
+};
+
+AUG_HEAD(aug_allocs32, aug_alloc32);
+
+struct aug_alloc64 {
+    AUG_ENTRY(aug_alloc64);
     char mem_[64];
 };
 
-AUG_HEAD(aug_allocs, aug_alloc);
+AUG_HEAD(aug_allocs64, aug_alloc64);
+
+AUGUTIL_API void
+aug_destroyallocs32(struct aug_allocs32* allocs32);
+
+AUGUTIL_API void
+aug_destroyalloc32(struct aug_alloc32* alloc32);
+
+AUGUTIL_API struct aug_alloc32*
+aug_createalloc32(void);
+
+AUGUTIL_API void
+aug_destroyallocs64(struct aug_allocs64* allocs64);
+
+AUGUTIL_API void
+aug_destroyalloc64(struct aug_alloc64* alloc64);
+
+AUGUTIL_API struct aug_alloc64*
+aug_createalloc64(void);
+
+AUGUTIL_API void
+aug_free(void* ptr);
+
+AUGUTIL_API void*
+aug_alloc(size_t size);
 
 #endif /* AUGUTIL_ALLOC_H */
