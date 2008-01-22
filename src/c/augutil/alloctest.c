ddf0973abef08710e95cf7c27c0ffd10c111e1c7
Mark Aylett <mark@emantic.co.uk>
2008-01-22 08:03:55 +0000

    Added alloc unit-test.

---
+++ src/c/augutil/alloctest.c
@@ -0,0 +1,16 @@
+/* Copyright (c) 2004-2007, Mark Aylett <mark@emantic.co.uk>
+   See the file COPYING for copying permission.
+*/
+#include "augsys.h"
+#include "augutil.h"
+
+#include <stdio.h>
+
+int
+main(int argc, char* argv[])
+{
+    struct aug_errinfo errinfo;
+    aug_atexitinit(&errinfo);
+
+    return 0;
+}
