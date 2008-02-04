95ae88845039371a6b718eb24264132c05b77ee5
Mark Aylett <mark@emantic.co.uk>
2008-02-04 14:30:40 +0000

    Moved declarations from types header.

--- src/c/augctx/types.h
+++ src/c/augctx/types.h
@@ -4,19 +4,6 @@
 #ifndef AUGCTX_TYPES_H
 #define AUGCTX_TYPES_H
 
-struct timeval;
-
-#if !defined(_WIN32)
-# include <sys/uio.h>
-typedef int aug_fd;
-#else /* _WIN32 */
-struct iovec {
-    void* iov_base;
-    int iov_len;
-};
-typedef void* aug_fd;
-#endif /* _WIN32 */
-
 enum aug_loglevel {
     AUG_LOGCRIT,
     AUG_LOGERROR,
@@ -33,14 +20,5 @@ struct aug_errinfo {
     int num_;
     char desc_[512];
 };
-
-#define AUG_SNTRUNCF(str, size, ret)            \
-    do {                                        \
-        (str)[(size) - 1] = '\0';               \
-        if ((int)(size) <= (ret))               \
-            ret = (int)(size) - 1;              \
-        else if ((ret) < 0 && 0 == errno)       \
-            errno = EINVAL;                     \
-    } while (0)
 
 #endif /* AUGCTX_TYPES_H */
