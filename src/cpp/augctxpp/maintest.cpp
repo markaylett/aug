fa294782d7f2f690e16a7017262557ee0d0045b7
Mark Aylett <mark@emantic.co.uk>
2008-09-02 07:15:31 +0100

    Added operator-new and -delete test.

--- src/cpp/augctxpp/maintest.cpp
+++ src/cpp/augctxpp/maintest.cpp
@@ -3,7 +3,34 @@
 */
 #include "augctxpp.hpp"
 
+#include <iostream>
+
 using namespace aug;
+using namespace std;
+
+namespace {
+    class mpool_base {
+    protected:
+        ~mpool_base()
+        {
+        }
+    public:
+        static void*
+        operator new(size_t size)
+        {
+            mpoolptr mpool(getmpool(aug_tlx));
+            return aug_allocmem(mpool.get(), size);
+        }
+        static void
+        operator delete(void* ptr)
+        {
+            mpoolptr mpool(getmpool(aug_tlx));
+            aug_freemem(mpool.get(), ptr);
+        }
+    };
+    class test : public mpool_base {
+    };
+}
 
 int
 main(int argc, char* argv[])
@@ -11,6 +38,8 @@ main(int argc, char* argv[])
     try {
 
         autobasictlx();
+        test* t = new test();
+        delete t;
         return 0;
 
     } AUG_PERRINFOCATCH;
