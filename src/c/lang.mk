ad1c9a1b579bf224a729be7b5992f024d4a601d5
Mark Aylett <mark.aylett@gmail.com>
2009-02-09 21:21:20 +0000

    Added Python and Ruby flags to configure.

--- src/c/lang.mk
+++ src/c/lang.mk
@@ -1,3 +1,4 @@
+ACLOCAL_AMFLAGS = -Im4
 DEFAULT_INCLUDES = \
 	-I$(top_srcdir)/src \
 	-I$(top_srcdir)/src/c
--- src/cpp/lang.mk
+++ src/cpp/lang.mk
@@ -1,5 +1,6 @@
 SUFFIXES = .cpp .hpp .idl
 
+ACLOCAL_AMFLAGS = -Im4
 DEFAULT_INCLUDES = \
 	-I$(top_srcdir)/src \
 	-I$(top_srcdir)/src/c \
