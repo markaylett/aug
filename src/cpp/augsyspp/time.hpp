b20549b0b6cb7439231a724426e322afe2e9dbe2
Mark Aylett <mark.aylett@gmail.com>
2009-05-23 13:33:39 +0100

    Added aug_time casts.

--- src/cpp/augsyspp/time.hpp
+++ src/cpp/augsyspp/time.hpp
@@ -45,7 +45,7 @@ namespace aug {
     inline tm&
     gmtime(tm& res)
     {
-        return gmtime(time(0), res);
+        return gmtime(static_cast<aug_time>(time(0)), res);
     }
 
     inline tm&
@@ -57,7 +57,7 @@ namespace aug {
     inline tm&
     localtime(tm& res)
     {
-        return localtime(time(0), res);
+        return localtime(static_cast<aug_time>(time(0)), res);
     }
 
     inline aug_timeval&
