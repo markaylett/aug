9a4abf6d43ae1cc7908f27251f636afeaf4761cb
Mark Aylett <mark.aylett@gmail.com>
2010-01-05 07:58:32 +0000

    Atomic updates.

--- src/c/augsys/barrier.h
+++ src/c/augsys/barrier.h
@@ -31,13 +31,13 @@
 
 #include "augctx/lock.h"
 
-#if !ENABLE_SMP
+#if defined(__GNUC__)
+# if !ENABLE_SMP
 /**
  * Prevent compiler re-ordering.
  */
-# define AUG_MB() __asm__ __volatile__("":::"memory")
-#else /* ENABLE_SMP */
-# if defined(__GNUC__)
+#  define AUG_MB() __asm__ __volatile__("":::"memory")
+# else /* ENABLE_SMP */
 #  if defined(__i386__) || defined(__i486__)
 #   define AUG_MB()  __asm__ __volatile__("lock; addl $0,0(%%esp)":::"memory")
 #   define AUG_WMB() __asm__ __volatile__("":::"memory")
@@ -58,7 +58,7 @@
 "membar #LoadLoad | #LoadStore | #StoreStore | #StoreLoad":::"memory")
 #   define AUG_RMB() __asm__ __volatile__("membar #LoadLoad":::"memory")
 #   define AUG_WMB() __asm__ __volatile__("membar #StoreStore":::"memory")
-#  elif (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)
+#  elif __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)
 /**
  * Use GCC's intrinsics.
  */
@@ -73,34 +73,34 @@
         aug_unlock();                             \
     } while (0)
 #  endif
-# elif defined(_MSC_VER)
-#  if (_MSC_VER >= 1400)
-#   include <intrin.h>
-#   pragma intrinsic(_ReadWriteBarrier)
-#   pragma intrinsic(_ReadBarrier)
-#   pragma intrinsic(_WriteBarrier)
-#   define AUG_MB()  _ReadWriteBarrier()
-#   define AUG_RMB() _ReadBarrier()
-#   define AUG_WMB() _WriteBarrier()
-#  else /* _MSC_VER < 1400 */
+# endif /* ENABLE_SMP */
+#elif defined(_MSC_VER)
+# if _MSC_VER >= 1400
+#  include <intrin.h>
+#  pragma intrinsic(_ReadWriteBarrier)
+#  pragma intrinsic(_ReadBarrier)
+#  pragma intrinsic(_WriteBarrier)
+#  define AUG_MB()  _ReadWriteBarrier()
+#  define AUG_RMB() _ReadBarrier()
+#  define AUG_WMB() _WriteBarrier()
+# else /* _MSC_VER < 1400 */
 AUG_EXTERNC void _ReadWriteBarrier(void);
-#   pragma intrinsic(_ReadWriteBarrier)
-#   define AUG_MB()  _ReadWriteBarrier()
-#  endif /* _MSC_VER < 1400 */
-# elif defined(__APPLE__) || defined(__MACH__)
-#  include <libkern/OSAtomic.h>
-#  define AUG_MB() OSMemoryBarrier()
-# else
+#  pragma intrinsic(_ReadWriteBarrier)
+#  define AUG_MB()  _ReadWriteBarrier()
+# endif /* _MSC_VER < 1400 */
+#elif defined(__APPLE__) || defined(__MACH__)
+# include <libkern/OSAtomic.h>
+# define AUG_MB() OSMemoryBarrier()
+#else
 /**
  * Implement in terms of posix lock.
  */
-#  define AUG_MB()                               \
+# define AUG_MB()                               \
     do {                                         \
         aug_lock();                              \
         aug_unlock();                            \
     } while (0)
-# endif
-#endif /* ENABLE_SMP */
+#endif
 
 /**
  * If not defined, define read barrier in terms of full barrier.
