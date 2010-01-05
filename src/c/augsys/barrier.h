eaff8095734e29e05eb5e2234c7ca1a66c88520a
Mark Aylett <mark.aylett@gmail.com>
2010-01-05 09:29:32 +0000

    Atomic updates.

--- src/c/augsys/barrier.h
+++ src/c/augsys/barrier.h
@@ -31,91 +31,127 @@
 
 #include "augctx/lock.h"
 
-#if defined(__GNUC__)
-# if !ENABLE_SMP
-/**
- * Prevent compiler re-ordering.
- */
-#  define AUG_MB() __asm__ __volatile__("":::"memory")
-# else /* ENABLE_SMP */
-#  if defined(__i386__) || defined(__i486__)
-#   define AUG_MB()  __asm__ __volatile__("lock; addl $0,0(%%esp)":::"memory")
-#   define AUG_WMB() __asm__ __volatile__("":::"memory")
-#  elif defined(__i586__) || defined(__i686__) || defined(__x86_64__)
-#   define AUG_MB()  __asm__ __volatile__("mfence":::"memory")
-#   define AUG_RMB() __asm__ __volatile__("lfence":::"memory")
-#   define AUG_WMB() __asm__ __volatile__("sfence":::"memory")
-#  elif defined(__ia64__)
-#   define AUG_MB()  __asm__ __volatile__("mf":::"memory")
-#  elif defined(__alpha__)
-#   define AUG_MB()  __asm__ __volatile__("mb":::"memory")
-#   define AUG_WMB() __asm__ __volatile__("wmb":::"memory")
-#  elif defined(__PPC__)
-#   define AUG_MB()  __asm__ __volatile__("sync":::"memory")
-#   define AUG_WMB() __asm__ __volatile__("eieio":::"memory")
-#  elif defined(__sparc__)
-#   define AUG_MB()  __asm__ __volatile__(
-"membar #LoadLoad | #LoadStore | #StoreStore | #StoreLoad":::"memory")
-#   define AUG_RMB() __asm__ __volatile__("membar #LoadLoad":::"memory")
-#   define AUG_WMB() __asm__ __volatile__("membar #StoreStore":::"memory")
-#  elif __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)
-/**
- * Use GCC's intrinsics.
- */
-#   define AUG_MB()  __sync_synchronize()
-#  else
-/**
- * Implement in terms of lock.
- */
-#   define AUG_MB()                               \
-    do {                                          \
-        aug_lock();                               \
-        aug_unlock();                             \
-    } while (0)
-#  endif
-# endif /* ENABLE_SMP */
-#elif defined(_MSC_VER)
+#if defined(_MSC_VER)
+
 # if _MSC_VER >= 1400
+
 #  include <intrin.h>
 #  pragma intrinsic(_ReadWriteBarrier)
 #  pragma intrinsic(_ReadBarrier)
 #  pragma intrinsic(_WriteBarrier)
-#  define AUG_MB()  _ReadWriteBarrier()
-#  define AUG_RMB() _ReadBarrier()
-#  define AUG_WMB() _WriteBarrier()
+#  define aug_mb()  _ReadWriteBarrier()
+#  define aug_rmb() _ReadBarrier()
+#  define aug_wmb() _WriteBarrier()
+
 # else /* _MSC_VER < 1400 */
+
 AUG_EXTERNC void _ReadWriteBarrier(void);
 #  pragma intrinsic(_ReadWriteBarrier)
-#  define AUG_MB()  _ReadWriteBarrier()
+#  define aug_mb()  _ReadWriteBarrier()
+
 # endif /* _MSC_VER < 1400 */
-#elif defined(__APPLE__) || defined(__MACH__)
+
+#elif defined(__APPLE__) && defined(__MACH__)
+
 # include <libkern/OSAtomic.h>
-# define AUG_MB() OSMemoryBarrier()
-#else
+# define aug_mb() OSMemoryBarrier()
+
+#elif defined(__GNUC__)
+
+# if !ENABLE_SMP
+
 /**
- * Implement in terms of posix lock.
+ * Prevent compiler re-ordering.
  */
-# define AUG_MB()                               \
+
+#  define aug_mb() __asm__ __volatile__("":::"memory")
+
+# else /* ENABLE_SMP */
+
+#  if defined(__i386__) || defined(__i486__)
+
+#   define aug_mb()  __asm__ __volatile__("lock; addl $0,0(%%esp)":::"memory")
+#   define aug_wmb() __asm__ __volatile__("":::"memory")
+
+#  elif defined(__i586__) || defined(__i686__) || defined(__x86_64__)
+
+#   define aug_mb()  __asm__ __volatile__("mfence":::"memory")
+#   define aug_rmb() __asm__ __volatile__("lfence":::"memory")
+#   define aug_wmb() __asm__ __volatile__("sfence":::"memory")
+
+#  elif defined(__ia64__)
+
+#   define aug_mb()  __asm__ __volatile__("mf":::"memory")
+
+#  elif defined(__alpha__)
+
+#   define aug_mb()  __asm__ __volatile__("mb":::"memory")
+#   define aug_wmb() __asm__ __volatile__("wmb":::"memory")
+
+#  elif defined(__PPC__)
+
+#   define aug_mb()  __asm__ __volatile__("sync":::"memory")
+#   define aug_wmb() __asm__ __volatile__("eieio":::"memory")
+
+#  elif defined(__sparc__)
+
+#   define aug_mb()  __asm__ __volatile__(
+"membar #LoadLoad | #LoadStore | #StoreStore | #StoreLoad":::"memory")
+#   define aug_rmb() __asm__ __volatile__("membar #LoadLoad":::"memory")
+#   define aug_wmb() __asm__ __volatile__("membar #StoreStore":::"memory")
+
+#  elif __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)
+
+/**
+ * Use GCC's intrinsics.
+ */
+
+#   define aug_mb()  __sync_synchronize()
+
+#  else
+
+/**
+ * Implement in terms of lock.
+ */
+
+#   define aug_mb()                               \
+    do {                                          \
+        aug_lock();                               \
+        aug_unlock();                             \
+    } while (0)
+
+#  endif
+
+# endif /* ENABLE_SMP */
+
+#else
+
+/**
+ * Implement in terms of lock.
+ */
+
+# define aug_mb()                               \
     do {                                         \
         aug_lock();                              \
         aug_unlock();                            \
     } while (0)
+
 #endif
 
 /**
  * If not defined, define read barrier in terms of full barrier.
  */
 
-#if !defined(AUG_RMB)
-# define AUG_RMB AUG_MB
-#endif /* !AUG_RMB */
+#if !defined(aug_rmb)
+# define aug_rmb aug_mb
+#endif /* !aug_rmb */
 
 /**
  * If not defined, define write barrier in terms of full barrier.
  */
 
-#if !defined(AUG_WMB)
-# define AUG_WMB AUG_MB
-#endif /* !AUG_WMB */
+#if !defined(aug_wmb)
+# define aug_wmb aug_mb
+#endif /* !aug_wmb */
 
 #endif /* AUGSYS_BARRIER_H */
